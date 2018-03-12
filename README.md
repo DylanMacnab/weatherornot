089e71f6bbd75390cfe79f1c51a6e95a9062830b
Dylan_Macnab <dylanmacnab@gmail.com>
2018-03-11 19:59:28 -0700

    creating render of current weather conditions

--- README.md
+++ README.md
@@ -2,4 +2,24 @@
 Simple practice app retrieving weather and attractions data from the APIXU and Foursquare public APIs.
 
 ## Features Ideas:
-Create a version of this app that targets activities based on the current weather or expected forecast.
+- add degrees to the temps
+- add featured Today section with a more detailed version of the weather
+- add more relevant details for the venues
+- create a more interesting layout for the temps
+- remove jQuery
+- use react or vue to build UI
+- add a predictive search using search.json
+
+
+
+// Notes:
+
+As a user I would like to be able to check the current forecast to find this information:
+
+- Chance of Rain
+- Current Temperature
+- Expected high and low
+- Clothing tip
+- Historical weather info:
+  - Record High
+  - Record Low
--- public/main.js
+++ public/main.js
@@ -7,6 +7,8 @@ const imgPrefix = 'https://igx.4sqi.net/img/general/150x200';
 // APIXU Info
 const apiKey = 'f0e5ebc0c2354f20bc761611180103';
 const forecastUrl = 'https://api.apixu.com/v1/forecast.json?key=';
+const currentUrl = 'https://api.apixu.com/v1/forecast.json?key=';
+
 
 // Page Elements
 const $input = $('#city');
@@ -16,20 +18,20 @@ const $container = $('.container');
 const $venueDivs = [$("#venue1"), $("#venue2"), $("#venue3"), $("#venue4")];
 const $weatherDivs = [$("#weather1"), $("#weather2"), $("#weather3"), $("#weather4"), $("#weather5"), $("#weather6"), $("#weather7")];
 const weekDays = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN'];
+const $currentWeather = $('#current');
 
 // AJAX functions
+
+// Get 4 Closest Venues
 async function getVenues() {
   const city = $input.val();
-  const urlToFetch = url + city + '&venuePhotos=1&limit=10&client_id=' + clientId + '&client_secret=' + clientSecret + '&v=20180306';
+  const urlToFetch = url + city + '&venuePhotos=1&limit=10&client_id=' + clientId + '&client_secret=' + clientSecret + '&v=20180311';
 
   try {
     let response = await fetch(urlToFetch);
     if (response.ok) {
-      // console.log(response);
       let jsonResponse = await response.json();
-      // console.log(jsonResponse);
    		let venues = jsonResponse.response.groups[0].items.map(location => location.venue);
-      console.log(venues);
 			return venues;
     }
   }
@@ -39,16 +41,35 @@ async function getVenues() {
 
 };
 
+// Get Current Weather Conditions
+async function getCurrentForecast() {
+  const urlToFetch = currentUrl + apiKey + '&q=' + $input.val();
+  console.log("Current Forecast URL " + urlToFetch);
+  try {
+    let response = await fetch(urlToFetch);
+    if (response.ok) {
+      let jsonResponse = await response.json();
+      let current = jsonResponse;
+      return current;
+    }
+  }
+  catch(error) {
+    console.log("error requesting current forecast")
+  }
+}
+
+
+// Get 7 Day Forecast
 async function getForecast() {
-  const urlToFetch = forecastUrl + apiKey + '&q=' + $input.val() + '&days=7&hour=12';
-	console.log(urlToFetch);
+  const urlToFetch = forecastUrl + apiKey + '&q=' + $input.val() + '&days=7';
+	//console.log(urlToFetch);
   try {
     let response = await fetch(urlToFetch);
     if (response.ok) {
       let jsonResponse = await response.json();
       console.log(jsonResponse);
       let days = jsonResponse.forecast.forecastday;
-      console.log(days);
+      //console.log(days);
       return days;
     }
 	}
@@ -75,8 +96,6 @@ function renderVenues(venues) {
 }
 
 function renderForecast(days) {
-  console.log("days comin up");
-  console.log(days);
   $weatherDivs.forEach(($day, index) => {
     let weatherContent =
       '<h2 class="weather-day">' + weekDays[(new Date(days[index].date)).getDay()] + '</h2>' +
@@ -89,6 +108,23 @@ function renderForecast(days) {
   });
 }
 
+function renderCurrent(current) {
+  let currentContent =
+  `<div class="current-weather">
+    <h2>Currently:</h2>
+    <img src="http://${current.current.condition.icon}">
+    <p>
+      Description: ${current.current.condition.text} <br>
+      Temp: ${current.current.temp_f} <br>
+      Wind: ${current.current.wind_mph} <br>
+      High: ${current.forecast.forecastday[0].maxtemp_f} <br>
+      Low: ${current.forecast.forecastday[0].mintemp_f} <br>
+      Rain: ${current.forecast.forecastday[0].day.totalprecip_mm} <br>
+    </p>
+  </div>`;
+  $currentWeather.append(currentContent);
+}
+
 function executeSearch() {
   $venueDivs.forEach(venue => venue.empty());
   $weatherDivs.forEach(day => day.empty());
@@ -99,6 +135,9 @@ function executeSearch() {
   });
   getForecast().then(days => {
     renderForecast(days);
+  });
+  getCurrentForecast().then(current => {
+    renderCurrent(current);
   });
   return false;
 }
